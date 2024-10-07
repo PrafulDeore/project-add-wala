f41c323c38889ba646c3e0bf07b9eb16c747795d
praful.deore <praful.deore@varomoney.com>
2024-10-07 11:09:10 +0530

    Intiala commit

---
+++ add-wala/app/(tabs)/profile.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+
+const Profile = () => {
+  return (
+    <ScrollView>
+      <Text>Profile</Text>
+    </ScrollView>
+  );
+};
+export default Profile;
